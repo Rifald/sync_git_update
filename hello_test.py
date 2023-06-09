aa2f3e263304e0fbdad8b2f478479282eaf1d17b
Rifaldi Bachtiar <rifaldi.bachtiar@gameloft.com>
2023-06-09 10:43:20 +0700

    commit 1

--- hello_test.py
+++ hello_test.py
@@ -6,7 +6,7 @@ canvas1 = tk.Canvas(root, width = 300, height = 300)
 canvas1.pack()
 
 def hello ():  
-    label1 = tk.Label(root, text= 'Hello World!', fg='blue', font=('helvetica', 12, 'bold'))
+    label1 = tk.Label(root, text= 'Hello World! v1.0', fg='blue', font=('helvetica', 12, 'bold'))
     canvas1.create_window(150, 200, window=label1)
     
 button1 = tk.Button(text='Click Me', command=hello, bg='brown',fg='white')
