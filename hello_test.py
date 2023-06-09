115db00ccc5fb3a5713bf529ae59d914f86ada2e
Rifaldi Bachtiar <rifaldi.bachtiar@gameloft.com>
2023-06-09 14:30:06 +0700

    three commit

--- hello_test.py
+++ hello_test.py
@@ -1,28 +1,40 @@
+import os
 import tkinter as tk
 import requests
-
-currentVersion = "v0.2"
-
+import git
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print(dir_path)
+currentVersion = "v0.3"
 import requests
 r = requests.get('https://raw.githubusercontent.com/Rifald/sync_git_update/main/version.txt')
 data = r.text
 
-if (data == currentVersion):
-    check =  "App is up to date!"
-else:
-    check = "App is not up to date!"
-
-
-root= tk.Tk()
-
-canvas1 = tk.Canvas(root, width = 300, height = 300)
-canvas1.pack()
-
 def hello ():  
     label1 = tk.Label(root, text= 'Hello World! v1.0 \n' + check, fg='blue', font=('helvetica', 12, 'bold'))
     canvas1.create_window(150, 200, window=label1)
-    
-button1 = tk.Button(text='Click Me', command=hello, bg='brown',fg='white')
-canvas1.create_window(150, 150, window=button1)
 
-root.mainloop()
+if (data == currentVersion):
+    check =  "App is up to date!"
+    root= tk.Tk()
+
+    canvas1 = tk.Canvas(root, width = 300, height = 300)
+    canvas1.pack()
+        
+    button1 = tk.Button(text='Click Me', command=hello, bg='brown',fg='white')
+    canvas1.create_window(150, 150, window=button1)
+
+    root.mainloop()
+else:
+    # Instantiate repo object
+    repo = git.Repo(dir_path)
+    # pull down 
+    repo.remotes.origin.pull()
+    
+    root= tk.Tk()
+    canvas1 = tk.Canvas(root, width = 300, height = 300)
+    canvas1.pack()
+    label1 = tk.Label(root, text= 'Please restart application', fg='blue', font=('helvetica', 12, 'bold'))
+    canvas1.create_window(150, 200, window=label1)
+    root.mainloop()
+
+
